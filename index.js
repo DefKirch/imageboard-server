5440a6a5dcc8412c3e2c48d5d92ba80367421d4a
DEFKIRCH <thijmen.kirch@gmail.com>
2022-06-02 14:08:07 +0200

    Added routers

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
-const app = express();
+
+// Router importss
+const userRouter = require("./routers/user");
+const imageRouter = require("./routers/image");
+
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+// Middlewares -> tools
+app.use(express.json()); // Body parser middleware
+
+// Routers
+app.use("/users", userRouter);
+// app.use("/images", imageRouter);
+
+// Start the app
 app.listen(PORT, () => console.log("Server started in port:", PORT));
