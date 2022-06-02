0608e6096828e633333140e02d9d7eaf2b41e352
DEFKIRCH <thijmen.kirch@gmail.com>
2022-06-02 10:53:27 +0200

    Initial commit, setup express

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// app.get
+
+app.listen(PORT, () => console.log("Server started in port:", PORT));
